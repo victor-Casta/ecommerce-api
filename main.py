2746d6fe5bb3402c8b63a3d144f460675491cee8
Victor Castañeda <victorarroyavecastaneda@gmail.com>
2024-09-26 21:03:27 -0500

    update products structure

--- main.py
+++ main.py
@@ -1,19 +1,81 @@
-from flask import Flask, jsonify, request
-app =  Flask(__name__)
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-@app.route('/')
-def hello():
-  return 'Hello, World!'
+app = Flask(__name__)
 
-@app.route('/products/<product_id>')
-def get_products(product_id):
-  products = [
-    {'id': product_id, 'name': 'Product 1', 'price': 19.99},
-  ]
-  query = request.args.get('query')
-  if query:
-    products.append(query)
-  return jsonify(products), 200
+CORS(app)
+
+# Lista de categorías
+categories = [
+  {
+    'id': 1,
+    'name': 'Shirts',
+    'products': ['T-Shirt']
+  },
+  {
+    'id': 2,
+    'name': 'Pants',
+    'products': ['Pants']
+  },
+  {
+    'id': 3,
+    'name': 'Outerwear',
+    'products': ['Jacket']
+  }
+]
+
+# Lista de productos
+products = [
+    {
+      'id': 1,
+      'name': 'T-Shirt',
+      'category': 'Shirts',
+      'price': 19.99,
+      'image': 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg'
+    },
+    {
+      'id': 2,
+      'name': 'Jeans',
+      'category': 'Pants',
+      'price': 39.99,
+      'image': 'https://images.pexels.com/photos/2897521/pexels-photo-2897521.jpeg'
+    },
+    {
+      'id': 3,
+      'name': 'Jacket',
+      'category': 'Outerwear',
+      'price': 59.99,
+      'image': 'https://images.pexels.com/photos/28561358/pexels-photo-28561358.jpeg'
+    }
+]
+
+# Ruta para obtener todas las categorías
+@app.route('/categories', methods=['GET'])
+def get_all_categories():
+    return jsonify(categories), 200
+
+# Ruta para obtener todos los productos
+@app.route('/products', methods=['GET'])
+def get_all_products():
+    return jsonify(products), 200
+
+# Ruta para obtener una categoría específica por su id
+@app.route('/categories/<int:category_id>', methods=['GET'])
+def get_category_by_id(category_id):
+    category = next((item for item in categories if item['id'] == category_id), None)
+    if category:
+        return jsonify(category), 200
+    else:
+        return jsonify({'error': 'Category not found'}), 404
+
+# Ruta para obtener un producto específico por su id
+@app.route('/products/<int:product_id>', methods=['GET'])
+def get_product_by_id(product_id):
+    product = next((item for item in products if item['id'] == product_id), None)
+    if product:
+        return jsonify(product), 200
+    else:
+        return jsonify({'error': 'Product not found'}), 404
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
